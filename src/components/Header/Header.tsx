945c048c2202d3d0eae60b5d824372caced2d428
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-09-25 16:43:27 +0500

    header-done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,66 +3,86 @@ import "./styles.module.sass";
 import React from "react";
 import { Input } from "../ui/input";
 import Link from "next/link";
+import { ChevronDown } from "lucide-react";
+
 const Header = () => {
   return (
     <>
-      <div className="flex justify-between align-center p-5">
-        <div className="flex align-center">
-          <img src="/logo.svg" alt="Логотип" className="w-auto mr-2" />
-          <button className="flex border-none cursor-pointer bg-transparent align-center uppercase font-inter-bold font-bold">
-            <img src="/location.svg" alt="" className="mr-2 w-[20]" /> Астана
-            <i className="arrow-down border-solid-"></i>
+      <div className="flex justify-between items-center py-5">
+        <div className="flex items-center">
+          <img src="/logo.svg" alt="Логотип" className="w-[100px] mr-2" />
+          <button className="flex border-none cursor-pointer bg-transparent justify-center items-center font-inter-bold font-bold mr-12  text-[13px]">
+            <img src="/location.svg" alt="" className="mr-2 w-[20px]" /> Астана
+            <ChevronDown />
           </button>
-          <button className="flex border-none cursor-pointer bg-transparent align-center uppercase font-inter-bold font-bold">
-            <img src="/ru.svg" alt="" className="mr-2 w-[20]" /> RU
-            <i className="arrow-down border border-black"></i>
+          <button className="flex border-none cursor-pointer bg-transparent justify-center items-center uppercase font-inter-bold font-bold mr-12 text-[13px]">
+            <img src="/ru.svg" alt="" className="mr-2 w-[20px]" /> RU
+            <ChevronDown />
           </button>
         </div>
-        <div className="flex align-center">
-          <div className="mr-[50px]">
-            <Link href="tel:87080016868">
-              <img
-                src="/phone.svg"
-                alt="phone"
-                className="mr-2 w-[20] mr-[50px]"
-              />
-              <span>8(708)001-68-68</span>
+        <div className="flex items-center">
+          <div>
+            <Link
+              href="tel:87080016868"
+              className="mr-[50px] text-[12px] flex items-center"
+            >
+              <img src="/phone.svg" alt="phone" className="w-[20px] mr-2" />
+              <b>8(708)001-68-68</b>
             </Link>
           </div>
-          <div className="mr-[50px]">
-            <img
-              src="/email.svg"
-              alt="email"
-              className="mr-2 w-[20] mr-[50px]"
-            />
-            <span>info@example.com</span>
+          <div>
+            <Link
+              href="mailto:info@example.com"
+              className="mr-[50px] text-[12px] flex items-center"
+            >
+              <img src="/email.svg" alt="email" className="w-[20px] mr-2" />
+              <b>info@example.com</b>
+            </Link>
           </div>
-          <button className="request-btn">
-            <img src="/request.svg" alt="" className="mr-2 w-[20]" /> Оставить
-            заявку
-          </button>
+          <Link href="#">
+            <button className="mr-[50px] text-[12px] flex items-center font-bold">
+              <img src="/request.svg" alt="" className="mr-2 w-[20px]" />
+              Оставить заявку
+            </button>
+          </Link>
         </div>
       </div>
 
-      <div className="divider"></div>
+      <div className="relative w-full h-[3px] bg-transparent">
+        <div className="absolute top-[-1px] left-0 right-0 h-[3px] bg-gradient-to-r from-transparent via-[#FFD66C] to-transparent"></div>
+      </div>
 
-      <div className="header-bottom">
-        <div className="flex align-center">
-          <button>
-            <img src="/catalog.svg" alt="" className="mr-2 w-[20]" /> Каталог
+      <div className="mt-8 flex justify-between items-center py-5">
+        <div className="flex items-center">
+          <button className="flex items-center font-bold">
+            <img
+              src="/catalog.svg"
+              alt=""
+              className="mr-2 w-[20px] text-[14px]"
+            />
+            Каталог
           </button>
-          <div className="search-bar relative flex align-center">
-            <Input type="text" placeholder="Найти товар" defaultValue="" className="ml-5 border-none outline-none"/>
-            <img src="/search-icon.svg" alt="Лупа" className="mr-2 absolute left-[30px] h-[16px] w-[16px]" />
+          <div className="relative flex items-center bg-[#F5F7F8] py-3 pr-[8px] pl-[25px] rounded-[8px] ml-12">
+            <Input
+              type="text"
+              placeholder="Найти товар"
+              defaultValue=""
+              className="ml-5 border-none outline-none"
+            />
+            <img
+              src="/search-icon.svg"
+              alt="Лупа"
+              className="mr-2 absolute left-[15px] h-[16px] w-[16px] text-[14px]"
+            />
           </div>
         </div>
-        <div className="flex align-center">
+        <div className="flex items-center">
           <div className="flex">
-            <button className="ml-5">Преимущества</button>
-            <button className="ml-5">Продукция</button>
-            <button className="ml-5">Вопросы</button>
-            <button className="ml-5">Контакты</button>
-            <button className="ml-5">
+            <button className="ml-5 text-[14px] font-bold">Преимущества</button>
+            <button className="ml-5 text-[14px] font-bold">Продукция</button>
+            <button className="ml-5 text-[14px] font-bold">Вопросы</button>
+            <button className="ml-5 text-[14px] font-bold">Контакты</button>
+            <button className="ml-5 text-[14px] font-bold">
               <img src="/cart.svg" alt="" className="mr-2 w-[30]" />
             </button>
           </div>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,7 @@ const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLI
       <input
         type={type}
         className={cn(
-          "flex h-5 w-[360px] rounded-[4px]  bg-background pl-[35px] pr-[50px] py-[5px] text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-5 w-[360px] rounded-[4px]  bg-[#F5F7F8] text-sm ring-offset-background file:border-0 file:bg-[#F5F7F8] file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
